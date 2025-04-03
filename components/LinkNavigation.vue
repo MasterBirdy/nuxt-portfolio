8386a8029af856029aeef76d51e9fce9568ba5f2
Matthew Ito <mito@Helenas-MBP.lan>
2025-04-02 21:16:48 -0700

    Add resume

--- components/LinkNavigation.vue
+++ components/LinkNavigation.vue
@@ -1,7 +1,14 @@
 <template>
     <nav class="mt-5 mx-auto">
         <div class="flex gap-6 flex-1">
-            <NuxtLink to="/" class="underline">Home</NuxtLink>
+            <NuxtLink to="/" class="text-gray-750">Home</NuxtLink>
+            <NuxtLink
+                to="/resume.pdf"
+                class="text-gray-750"
+                target="_blank"
+                external
+                >Resume</NuxtLink
+            >
         </div>
     </nav>
 </template>
--- layouts/Default.vue
+++ layouts/Default.vue
@@ -4,7 +4,9 @@
     <div class="container mt-10 w-3/4 mx-auto">
         <h1 class="text-4xl font-bold">Matthew Ito</h1>
         <LinkNavigation />
-        <p class="mt-5">Software developer, husband, soon to be father. 👶</p>
+        <p class="mt-5 text-gray-700">
+            Software developer, husband, soon to be father. 👶
+        </p>
         <slot />
     </div>
 </template>
