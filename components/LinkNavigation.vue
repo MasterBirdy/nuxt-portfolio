b701e93fd771a71a883b4112a1311a505a499bd2
Matthew Ito <matthew.ito@grantstreet.com>
2026-01-27 20:05:08 -0800

    Remove resume

--- components/LinkNavigation.vue
+++ components/LinkNavigation.vue
@@ -2,13 +2,6 @@
     <nav class="mt-5 mx-auto">
         <div class="flex gap-6 flex-1">
             <NuxtLink to="/" class="text-gray-750">Home</NuxtLink>
-            <NuxtLink
-                to="/resume.pdf"
-                class="text-gray-750"
-                target="_blank"
-                external
-                >Resume</NuxtLink
-            >
         </div>
     </nav>
 </template>
